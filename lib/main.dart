dd12049d063bd0d7f34faff00b5cd9129bf67c43
sejalpekam <sejalpekam@gmail.com>
2021-08-13 11:05:28 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Phone App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
